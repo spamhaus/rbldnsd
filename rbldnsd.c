054d1bbf4796d5bf401defc43a49db3dc91404b2
Michael Tokarev <mjt@tls.msk.ru>
2003-05-08 13:16:17 +0000

    fixed def_rr to be 127.0.0.2

--- rbldnsd.c
+++ rbldnsd.c
@@ -65,7 +65,7 @@ static char *logfile;		/* log file name */
 static struct ip4list *logfilt;	/* log filter: IPs/nets to log */
 static struct ip4list *qryfilt;	/* which IPs/nets will be serviced */
 static int logmemtms;		/* print memory usage and (re)load time info */
-const char def_rr[5] = "\1\0\0\177\0";
+const char def_rr[5] = "\2\0\0\177\0";
 
 /* a list of zonetypes. */
 const struct dataset_type *dataset_types[] = {
