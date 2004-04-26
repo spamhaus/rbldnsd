e477ff8d68f5cfb1a25c7c74d1b0d78e20ba6ebf
Michael Tokarev <mjt@tls.msk.ru>
2004-04-26 09:30:34 +0000

    (almost) completed the example hook (untested)

--- rbldnsd_hooks.c
+++ rbldnsd_hooks.c
@@ -15,6 +15,8 @@
 #include <time.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <syslog.h>
 
 /* example of implementation:
  * we're logging positive queries made by a list of trusted clients.
@@ -29,6 +31,9 @@ static char *logfn;
 
 static ip4addr_t *cliip;
 static unsigned ncliip;
+static unsigned acliip;
+
+#define STEP 64
 
 int hook_getopt(char *optarg) {
   if (memcmp(optarg, "log:", 4) == 0) logfn = optarg + 4;
@@ -54,33 +59,78 @@ void hook_query_result(const struct sockaddr *cli, const struct zone *zone,
     else if (cliip[m] < q) a = m + 1;
     else break;
   }
+  /* note: in case of excessive amount of queries,
+   * the file may grow pretty quickly.
+   */
   a = open(logfn, O_WRONLY|O_APPEND|O_CREAT, 0644);
   if (a < 0) return;
-  stpcpy(stpcpy(s, ip4atos(qi->qi_ip4)), "\n");
-  write(a, s, strlen(s));
+  m = sprintf(s, "%s\n", ip4atos(qi->qi_ip4));
+  write(a, s, m);
   close(a);
 }
 
 static int read_cli_list(FILE *f) {
   char buf[512];
   ip4addr_t a;
-  char *p;
-  /* read lines from f, parse IP addresses and add them to cliip array;
-   * qsort it at the end, #including "qsort.h" */
-  return 0;
+  char *p, *e;
+  int lineno = 0;
+  int warns = 0;
+  int ret = 0;
+  unsigned ne = 0;
+  unsigned ae = acliip;
+
+  while(fgets(buf, sizeof(buf), f)) {
+    ++lineno;
+    p = buf;
+    while(*p == ' ' || *p == '\t') ++p;
+    if (*p == '#' || *p == '\n') continue;
+    if (ip4addr(p, &a, &e) < 0) {
+      if (warns++ < 5)
+        dslog(LOG_WARNING, 0, "%.50s:%d: invalid IP address", clifn, lineno);
+      continue;
+    }
+    if (ne >= ae) {
+      ip4addr_t *newcli =
+        (ip4addr_t*)erealloc(cliip, (ae + STEP) * sizeof(ip4addr_t));
+      if (!newcli) {
+        dslog(LOG_WARNING, 0,
+              "unable to read list of client IPs: out of memory");
+	ret = -1;
+	break;
+      }
+      cliip = newcli;
+      ae += STEP;
+    }
+    cliip[ne++] = a;
+  }
+  if (ne) {
+    ae = (ne + STEP - 1) / STEP * STEP;
+#   define QSORT_TYPE ip4addr_t
+#   define QSORT_BASE cliip
+#   define QSORT_NELT ne
+#   define QSORT_LT(a,b) *a < *b
+#   include "qsort.c"
+#   define eeq(a,b) a == b
+    REMOVE_DUPS(ip4addr_t, cliip, ne, eeq);
+    SHRINK_ARRAY(ip4addr_t, cliip, ne, ae);
+  }
+  ncliip = ne;
+  acliip = ae;
+  return ret;
 }
 
 int hook_reload(const struct zone *zonelist) {
   struct stat st;
   FILE *f;
+  int ret;
   if (stat(clifn, &st) < 0) return -1;
   if (st.st_mtime == clifn_timestamp) return 0;
   start_loading();
   f = fopen(clifn, "r");
   if (f == NULL) return -1;
-  read_cli_list(f);
+  ret = read_cli_list(f);
   fclose(f);
-  return 0;
+  return ret;
 }
 
 #else /* ! TRUSTED_QUERY_LOGGING */
