7ccbe17dc0c5c9a2c1590f8b6a165f498ab21c66
Michael Tokarev <mjt@tls.msk.ru>
2005-04-28 20:31:13 +0000

    fixed wrong section in addrr_ns() again

--- rbldnsd_packet.c
+++ rbldnsd_packet.c
@@ -836,7 +836,7 @@ static int addrr_ns(struct dnspacket *pkt, const struct zone *zone, int auth) {
   else if (!dnc_final(pkt, zns->data, zns->nssize, zns->jump, zns->nsjend))
     return 0;
   else
-    pkt->p_buf[auth ? p_nscnt2 : p_arcnt2] += zone->z_nns;
+    pkt->p_buf[auth ? p_nscnt2 : p_ancnt2] += zone->z_nns;
   /* pick up next variation of NS ordering */
   ++cns;
   if (cns >= zone->z_nns)
